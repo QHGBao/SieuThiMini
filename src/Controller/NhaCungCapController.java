2cf4b0f2c5c5ad8bdb33a3c298511c58eb04d633
nhtbao2491 <nhtbao2491@gmail.com>
2024-11-23 16:29:05 +0700

    Cập nhật code 23/11/2024 Thiên Bảo

--- src/Controller/NhaCungCapController.java
+++ src/Controller/NhaCungCapController.java
@@ -1,16 +1,27 @@
 package Controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import BUS.NhaCungCapBUS;
 import DTO.NhaCungCapDTO;
 
 public class NhaCungCapController implements Initializable{
@@ -21,10 +32,19 @@ public class NhaCungCapController implements Initializable{
     private ImageView btnDelete;
 
     @FXML
-    private ImageView btnReset;
+    private ImageView btnRepair;
 
     @FXML
     private ImageView btnSearch;
+
+    @FXML
+    private Pane containAdd;
+
+    @FXML
+    private Pane containDelete;
+
+    @FXML
+    private Pane containRepair;
 
     @FXML
     private TableColumn<NhaCungCapDTO, String> colDiaChi;
@@ -47,27 +67,51 @@ public class NhaCungCapController implements Initializable{
     @FXML
     private TextField txtInput;
 
-    
+    NhaCungCapBUS nccBus = new NhaCungCapBUS();
+    ObservableList<NhaCungCapDTO> dsNhaCungCap = FXCollections.observableArrayList(nccBus.getAllNCC());
+
+    public void loadDataNCC(){
+        colMaNCC.setCellValueFactory(new PropertyValueFactory<NhaCungCapDTO, String>("maNCC"));
+        colTenNCC.setCellValueFactory(new PropertyValueFactory<NhaCungCapDTO, String>("tenNCC"));
+        colDiaChi.setCellValueFactory(new PropertyValueFactory<NhaCungCapDTO, String>("diaChi"));
+        colSdt.setCellValueFactory(new PropertyValueFactory<NhaCungCapDTO, Integer>("sdt"));
+        colNguoiLH.setCellValueFactory(new PropertyValueFactory<NhaCungCapDTO, String>("nguoiLH"));
+        tableNCC.setItems(dsNhaCungCap);
+    }
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        // TODO Auto-generated method stub
-        
+        loadDataNCC();
     }
 
+    private Stage popupStage;
     @FXML
     void btnAddClicked(MouseEvent event) {
-
+        if (popupStage != null && popupStage.isShowing())
+            return;
+        try {   
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/formNCC.fxml"));
+            Parent parent = loader.load();
+            Scene scene = new Scene(parent);
+            popupStage = new Stage();
+            popupStage.setScene(scene);
+            popupStage.initStyle(StageStyle.UNDECORATED);
+            popupStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void btnAddEntered(MouseEvent event) {
-
+        String currentStyle = containAdd.getStyle();
+        containAdd.setStyle(currentStyle + "-fx-background-color: #efe8d4;");
     }
 
     @FXML
     void btnAddExited(MouseEvent event) {
-
+        String currentStyle = containAdd.getStyle();
+        containAdd.setStyle(currentStyle + "-fx-background-color: transparent;");
     }
 
     @FXML
@@ -77,31 +121,37 @@ public class NhaCungCapController implements Initializable{
 
     @FXML
     void btnDeleteEntered(MouseEvent event) {
-
+        String currentStyle = containDelete.getStyle();
+        containDelete.setStyle(currentStyle + "-fx-background-color: #efe8d4;");
     }
 
     @FXML
     void btnDeleteExited(MouseEvent event) {
+        String currentStyle = containDelete.getStyle();
+        containDelete.setStyle(currentStyle + "-fx-background-color: transparent;");
+    }
+
+     @FXML
+    void btnRepairClicked(MouseEvent event) {
 
     }
 
     @FXML
-    void btnResetClicked(MouseEvent event) {
-
+    void btnRepairEntered(MouseEvent event) {
+        String currentStyle = containRepair.getStyle();
+        containRepair.setStyle(currentStyle + "-fx-background-color: #efe8d4;");
     }
 
     @FXML
-    void btnResetEntered(MouseEvent event) {
-
-    }
-
-    @FXML
-    void btnResetExited(MouseEvent event) {
-
+    void btnRepairExited(MouseEvent event) {
+        String currentStyle = containRepair.getStyle();
+        containRepair.setStyle(currentStyle + "-fx-background-color: transparent;");
     }
 
     @FXML
     void btnSearchClicked(MouseEvent event) {
 
     }
+
 }
+
--- src/Controller/formNCCController.java
+++ src/Controller/formNCCController.java
@@ -1,23 +1,49 @@
 package Controller;
 
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class formNCCController {
+
     @FXML
     private Label btnCancel;
+
+    @FXML
+    private ImageView btnCross;
 
     @FXML
     private Label btnSave;
 
     @FXML
-    void btnCancelClicked(MouseEvent event) {
+    private Label warnDiachi;
 
+    @FXML
+    private Label warnNguoiLH;
+
+    @FXML
+    private Label warnSdt;
+
+    @FXML
+    private Label warnTen;
+
+    @FXML
+    void btnCancelClicked(MouseEvent event) {
+        Stage stage = (Stage) btnCancel.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
     void btnSaveClicked(MouseEvent event) {
 
+    }
+
+    @FXML
+    void btnCloseForm(MouseEvent event) {
+        Stage stage = (Stage) btnCancel.getScene().getWindow();
+        stage.close();
     }
 }
